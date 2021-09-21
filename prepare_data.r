84be37f137ade7c9ef44efcc9bf82c1e52f28bab
JS <shin.jihyung@gmail.com>
2021-09-21 16:21:21 -0400

    use 'mutate' instead of '$'

--- prepare_data.r
+++ prepare_data.r
@@ -17,9 +17,7 @@ library(arsenal)
 library(here)
 library(mgsub)
 library(psych)
-
 wd = '~/OneDrive - SickKids/ukbb_insulin_resistance/'
-
 
 ## ----load_data-------------------------------------------------------------------------------------------------------------------------------
 setwd(wd)
@@ -85,6 +83,17 @@ idata$SBP0 = apply(data.frame(subset(idata,select=c(SBP0.1,SBP0.2))),1,mean,na.r
 idata$DBP0 = apply(data.frame(subset(idata,select=c(DBP0.1,DBP0.2))),1,mean,na.rm=T)
 idata$SBP1 = apply(data.frame(subset(idata,select=c(SBP1.1,SBP1.2))),1,mean,na.rm=T)
 idata$DBP1 = apply(data.frame(subset(idata,select=c(DBP1.1,DBP1.2))),1,mean,na.rm=T)
-idata$Age.c = idata$Age_MRI - mean(idata$Age_MRI)
-idata$Time = idata$Age_MRI - idata$age_base
-idata$Time.c = idata$Time - mean(idata$Time)
+idata = idata %>% mutate(Age.c = Age_MRI - mean(Age_MRI),
+                         Time = Age_MRI - age_base,
+                         Time.c = Time - mean(Time))
+
+# Participant characteristic table ------------------------------------------------------
+tab1 = CreateTableOne(data = subset(idata,select=-eid),strata = 'Sex')
+print(tab1,nonnormal = c('Age_MRI','BMI_recruitment','BMI_adjAge',
+                         'SBP',"SBP_adjAge",
+                         'DBP',"DBP_adjAge",
+                         'HbA1c2','HbA1c_adjAge',
+                         'WMH','WMH_adjAge',
+                         "ICV","ICV_adjAge",
+                         'logWMH','logWMH_adjAge',
+                         "MCT","MCT_adjAge"))
