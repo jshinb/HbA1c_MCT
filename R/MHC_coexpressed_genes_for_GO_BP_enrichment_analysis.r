4607bd4fa879bab58d4a2fa8fe52621875af7c23
JShin <shin.jihyung@gmail.com>
2021-12-09 18:08:57 -0500

    minor change

--- R/MHC_coexpressed_genes_for_GO_BP_enrichment_analysis.r
+++ R/MHC_coexpressed_genes_for_GO_BP_enrichment_analysis.r
@@ -23,8 +23,13 @@ get_cor_pearson = function(i) {
 
 get_cor_spearman_pvalue = function(x,i) {
   y=unlist(subset(t_gene_expression,select=select.cols[i]))
-  pval = cor.test(x,y,method="s")$
-    p.value
+  pval = cor.test(x,y,method="s")$p.value
+  pval
+}
+
+get_cor_pearson_pvalue = function(x,i) {
+  y=unlist(subset(t_gene_expression,select=select.cols[i]))
+  pval = cor.test(x,y,method="p")$p.value
   pval
 }
 
@@ -89,23 +94,85 @@ cormat.test = data.table(ensembl_gene_id=gene_rows_adult_cortex$ensembl_gene_id,
 subset(cormat.test,gene_symbol %in% gene.rows.test$gene_symbol)
 names(cormat.test)[-c(1:2)] = gene.rows.test$gene_symbol
 
-#
+# spearman correlation p-values ------------------------------------------
 cormat.test.p = apply(t_gene_expression,2,get_cor_spearman_pvalue,i=1)
 for(i in 2:nrow(gene.rows.test)){
   print(i)
   tmp.p = apply(t_gene_expression,2,get_cor_spearman_pvalue,i=i)
   cormat.test.p = cbind(cormat.test.p,tmp.p)
 }
+dim(cormat.test.p)
 
-# cormat.test.p = data.table(t(cormat.test.p))
-# cormat.test.p = data.table(ensembl_gene_id=gene_rows_adult_cortex$ensembl_gene_id,
-#                          gene_symbol=gene_rows_adult_cortex$gene_symbol,
-#                          cormat.test)
+cormat.test.p = data.table(cormat.test.p)
+names(cormat.test.p) = gene.rows.test$gene_symbol
+
+cormat.test.p = data.table(ensembl_gene_id = gene_rows_adult_cortex$ensembl_gene_id,
+                           gene_symbol = gene_rows_adult_cortex$gene_symbol,
+                           cormat.test.p)
 
 subset(cormat.test.p,gene_symbol %in% gene.rows.test$gene_symbol)
 names(cormat.test.p)[-c(1:2)] = gene.rows.test$gene_symbol
+spearman.cor.p = cormat.test.p
+rm(cormat.test.p)
 
-#--------------- approach 2
+# pearson correlation p-values ------------------------------------------
+cormat.test.p = apply(t_gene_expression,2,get_cor_pearson_pvalue,i=1)
+for(i in 2:nrow(gene.rows.test)){
+  print(i)
+  tmp.p = apply(t_gene_expression,2,get_cor_pearson_pvalue,i=i)
+  cormat.test.p = cbind(cormat.test.p,tmp.p)
+}
+dim(cormat.test.p)
+
+cormat.test.p = data.table(cormat.test.p)
+names(cormat.test.p) = gene.rows.test$gene_symbol
+
+cormat.test.p = data.table(ensembl_gene_id = gene_rows_adult_cortex$ensembl_gene_id,
+                           gene_symbol = gene_rows_adult_cortex$gene_symbol,
+                           cormat.test.p)
+
+subset(cormat.test.p,gene_symbol %in% gene.rows.test$gene_symbol)
+names(cormat.test.p)[-c(1:2)] = gene.rows.test$gene_symbol
+pearson.cor.p = cormat.test.p
+
+# adjust p-values -------------------------------------------------------------
+included.genes = c("HLA-DRB1", "HLA-DRB5", "HLA-DQB2", "HLA-DQA2")
+p.FDR.spearman = p.adjust(unlist(subset(spearman.cor.p,select=included.genes)))
+p.FDR.pearson = p.adjust(unlist(subset(pearson.cor.p,select=included.genes)))
+
+p.FDR.spearman = matrix(p.FDR.spearman,ncol=4,byrow=F)
+p.FDR.pearson = matrix(p.FDR.pearson,ncol=4,byrow=F)
+colnames(p.FDR.spearman) <- colnames(p.FDR.pearson) <- included.genes
+rownames(p.FDR.spearman) <- rownames(p.FDR.pearson) <- cormat.test.p$gene_symbol
+p.FDR.spearman = data.table(ensembl_gene_id=cormat.test.p$ensembl_gene_id,gene_symbol = cormat.test.p$gene_symbol,p.FDR.spearman)
+p.FDR.pearson = data.table(ensembl_gene_id=cormat.test.p$ensembl_gene_id,gene_symbol = cormat.test.p$gene_symbol,p.FDR.pearson)
+
+FDR.siglevel = 0.05
+#gi=1; 
+for(gi in 1:4){
+  genei = included.genes[gi];print(genei)
+  
+  ind = cormat.test[[genei]] >0;print(sum(ind))
+  ind = ind & p.FDR.pearson[[genei]] <FDR.siglevel;print(sum(ind))#1917,328,742,163
+  ind = ind & cormat.test$gene_symbol != genei;print(sum(ind))
+  ind_pos = ind
+  
+  ofile_pos = paste(wd,"PANTHER_HLAgenes/pos_coexp_",genei,".tsv",sep='')
+  out_pos = data.table(gene=cormat.test$ensembl_gene_id[ind_pos],logp=-log10(p.FDR.pearson[[genei]][ind_pos]))
+  out_pos = out_pos[order(out_pos$logp,decreasing=T),]
+  write_tsv(out_pos,file=ofile_pos)
+  
+  ind = cormat.test[[genei]] <0;print(sum(ind))
+  ind = ind & p.FDR.pearson[[genei]] <FDR.siglevel;print(sum(ind))#916,241,455
+  ind_neg = ind
+  
+  ofile_neg = paste(wd,"PANTHER_HLAgenes/neg_coexp_",genei,".tsv",sep='')
+  out_neg = data.table(gene=cormat.test$ensembl_gene_id[ind_neg],logp=-log10(p.FDR.pearson[[genei]][ind_neg]))
+  out_neg = out_neg[order(out_neg$logp,decreasing=T),]
+  write_tsv(out_neg,file=ofile_neg)
+}
+
+# approach 2 ------------------------------------------------------------------
 library(tidyr)
 cormat_long <- gather(cormat.test, key=MHC_genes, value = r, `HLA-DRB1`:`HLA-DQA2`,factor_key=FALSE)
 head(cormat_long )
